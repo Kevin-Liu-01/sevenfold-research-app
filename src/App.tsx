11730e690a431dbace47b8a66fa4a36d1c1f1aba
Athan Zhang <athanzxyt@gmail.com>
2025-07-06 16:00:55 -0400

    refactor: marketing split

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,11 @@ import {
 
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import WorkbenchPage from "./pages/WorkbenchPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import NewProjectPage from "./pages/NewProjectPage";
-import Terms from "./pages/Terms";
-import Privacy from "./pages/Privacy";
 
 function App() {
   return (
@@ -24,15 +21,12 @@ function App() {
       <Router>
         <Routes>
           {/* Public routes */}
-          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
 
           {/* Protected routes */}
           <Route element={<ProtectedRoute />}>
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/project/:projectId" element={<WorkbenchPage />} />
             <Route path="/newproject" element={<NewProjectPage />} />
           </Route>
