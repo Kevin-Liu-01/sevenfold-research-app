dbb546b00c4d338ded80d48de291d202fef799e6
R0hit-0 <rohitkamath2006@gmail.com>
2025-07-27 20:22:29 -0400

    Update App.tsx

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   Navigate,
+  useNavigate,
 } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
@@ -13,29 +14,47 @@ import HomePage from "./pages/HomePage";
 import WorkbenchPage from "./pages/WorkbenchPage";
 import NewProjectPage from "./pages/NewProjectPage";
 import { ForgotPasswordPage, SigninPage, SignupPage } from "./pages/AuthPages";
+import { ChatProvider } from "./context/ChatContext";
+import ChatPage from "./pages/ChatPage";
+import MainLayout from "./pages/MainLayout";
 
 function App() {
+  // Placeholder sidebarProps, replace with real props as needed
+  const sidebarProps = {
+    activeViewer: "search",
+    setActiveViewer: () => {},
+    sourcePapers: [],
+    candidatePapers: [],
+    onPaperSelect: () => {},
+    selectedPaperId: null,
+    onCreateDocument: () => {},
+  };
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/signin" element={<SigninPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+    <ChatProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            {/* Public routes */}
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
-          {/* Protected routes */}
-          <Route element={<ProtectedRoute />}>
+            {/* Home page without sidebar */}
             <Route path="/home" element={<HomePage />} />
-            <Route path="/project/:projectId" element={<WorkbenchPage />} />
-            <Route path="/newproject" element={<NewProjectPage />} />
-          </Route>
 
-          {/* Catch all route */}
-          <Route path="*" element={<Navigate to="/home" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+            {/* Main layout for pages that need sidebar */}
+            <Route element={<MainLayout sidebarProps={sidebarProps} />}>
+              <Route path="/project/:projectId" element={<WorkbenchPage />} />
+              <Route path="/newproject" element={<NewProjectPage />} />
+              <Route path="/chat" element={<ChatPage />} />
+            </Route>
+
+            {/* Catch all route */}
+            <Route path="*" element={<Navigate to="/home" replace />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ChatProvider>
   );
 }
 
